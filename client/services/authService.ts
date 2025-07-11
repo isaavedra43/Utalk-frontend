84b62c5763faaf3d9c4a9705c39d091269082361
isaavedra43 <53502011+isaavedra43@users.noreply.github.com>
2025-07-11 12:57:39 -0600

    1

--- client/services/authService.ts
+++
@@ -1,7 +0,0 @@
-import { signInWithEmailAndPassword } from "firebase/auth"
-import { auth } from "@lib/firebaseClient"
-
-export async function login(email: string, password: string): Promise<string> {
-  const userCred = await signInWithEmailAndPassword(auth, email, password)
-  return userCred.user.getIdToken()
-} 
